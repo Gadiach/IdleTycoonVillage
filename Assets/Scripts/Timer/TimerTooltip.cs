2f8c4988eba364ec3a314995db5c50f88f9cca3a
Gadiach <mikhailamelin22@gmail.com>
2025-11-13 18:32:50 +0200

    Made Timer to work in Auto and NonAuto state

--- Assets/Scripts/Timer/TimerTooltip.cs
+++ Assets/Scripts/Timer/TimerTooltip.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,12 @@ public class TimerTooltip : MonoBehaviour
     [SerializeField] private float timeToEnd;
     [SerializeField] private GameObject incomeObject;
     DateTime finishTime;
+    bool isActiveBusiness = false;
+    private BuildingData buildingData;
+
+    [Header("Slider Colors")]
+    [SerializeField] private Color manualColor = new Color(1f, 0.64f, 0f); 
+    [SerializeField] private Color autoColor = Color.green;
 
     private void Start()
     {
@@ -26,6 +33,21 @@ public class TimerTooltip : MonoBehaviour
             slider.maxValue = (float)(finishTime - DateTime.Now).TotalSeconds;
             slider.value = 0f; 
         }
+
+        buildingData = GetComponentInParent<BuildingData>();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.Instance.AddListener<BuildingAutomationChangedGameEvent>(OnAutomationChanged);
+    }
+
+    private void OnDisable()
+    {
+        if (EventManager.Instance == null)
+            return;
+
+        EventManager.Instance.RemoveListener<BuildingAutomationChangedGameEvent>(OnAutomationChanged);
     }
 
     private void Update()
@@ -34,6 +56,17 @@ public class TimerTooltip : MonoBehaviour
     }
 
     public void ResetTimer()
+    {       
+        if(!isActiveBusiness)
+        {
+            RestartTimerLogic();
+            ResetSliderUI();
+        }
+
+        SetIncomeBtn(false);
+    }
+
+    private void RestartTimerLogic()
     {
         finishTime = DateTime.Now.AddSeconds(timeToEnd);
 
@@ -42,17 +75,20 @@ public class TimerTooltip : MonoBehaviour
             timer.Initialize(finishTime);
             timer.StartTimer();
         }
+    }
 
-        if (slider != null)
-        {
-            slider.maxValue = timeToEnd;
-            slider.value = 0f;
-        }
+    private void ResetSliderUI()
+    {
+        if (slider == null)
+            return;
 
-        if (incomeObject != null)
-        {
-            incomeObject.SetActive(false);
-        }
+        slider.maxValue = timeToEnd;
+        slider.value = 0f;
+    }
+
+    private void SetIncomeBtn(bool state)
+    {
+        incomeObject.SetActive(state);
     }
 
     private void UpdateTimerProgress()
@@ -61,12 +97,39 @@ public class TimerTooltip : MonoBehaviour
         slider.value = slider.maxValue - remainingTime;
     }
 
+    private void OnAutomationChanged(BuildingAutomationChangedGameEvent evt)
+    {
+        if (buildingData == evt.Building)
+        {
+            isActiveBusiness = evt.IsAutomated;
+            Debug.Log($"[TimerTooltip] Automation changed for {evt.Building.Name}: {isActiveBusiness}");
+            UpdateSliderColor();
+        }
+    }
+
+    private void UpdateSliderColor()
+    {
+        if (slider == null)
+            return;
+
+        Image fillImage = slider.fillRect?.GetComponent<Image>();
+
+        if (fillImage != null)
+        {
+            fillImage.color = isActiveBusiness ? autoColor : manualColor;
+        }
+    }
+
     private void OnTimerFinished()
     {
-        incomeObject.SetActive(true);
+        SetIncomeBtn(true);
 
         slider.value = slider.maxValue;
 
-        Debug.Log("[TimerTooltip] Timer finished! Income object activated.");
+        if (isActiveBusiness)
+        {
+            RestartTimerLogic();
+            ResetSliderUI();
+        }     
     }
 }
