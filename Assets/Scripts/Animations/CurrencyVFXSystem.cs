17294127d76f92426cfe06605d4a39c588ccc669
Gadiach <mikhailamelin22@gmail.com>
2026-08-25 21:19:17 +0300

    Animation for claim reward from missions

--- Assets/Scripts/Animations/CurrencyVFXSystem.cs
+++ Assets/Scripts/Animations/CurrencyVFXSystem.cs
@@ -1,13 +1,18 @@
+using System;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CurrencyVFXSystem : MonoBehaviour
 {
+    private readonly Dictionary<RectTransform, Vector3> targetOriginalScales = new();
+
     [Header("References")]
     [SerializeField] private CurrencyIconDatabase currencyIconDatabase;
     [SerializeField] private RectTransform flyingCurrencyPrefabsContainer;
     [SerializeField] private GameObject flyingCurrencyIconPrefab;
+    [SerializeField] private CurrencyVFXTarget[] currencyTargets;
 
     [Header("Animation")]
     [SerializeField] private int flyingIconCount = 8;
@@ -28,52 +33,52 @@ public class CurrencyVFXSystem : MonoBehaviour
         EventManager.Instance.RemoveListener<CurrencyClaimedEvent>(OnCurrencyClaimed);
     }
 
+    private void Awake()
+    {
+        foreach (CurrencyVFXTarget target in currencyTargets)
+        {
+            if (target.targetIcon != null)
+            {
+                targetOriginalScales[target.targetIcon] =
+                    target.targetIcon.localScale;
+            }
+        }
+    }
+
     private void OnCurrencyClaimed(CurrencyClaimedEvent info)
     {
         Sprite currencyIcon = currencyIconDatabase.GetIcon(info.CurrencyType);
 
-        if (currencyIcon == null)
+        RectTransform targetIcon = GetTargetIcon(info.CurrencyType);
+
+        if (currencyIcon == null || targetIcon == null)
             return;
 
-        Play(info.SourcePosition, currencyIcon);
+        Play(info.SourcePosition,currencyIcon,targetIcon);
     }
 
-    private void Play(Vector3 startPosition, Sprite currencyIcon)
+    private void Play(Vector3 startPosition,Sprite currencyIcon,RectTransform targetIcon)
     {
         for (int i = 0; i < flyingIconCount; i++)
         {
-            SpawnCurrencyIcon(startPosition, currencyIcon, i);
+            SpawnCurrencyIcon(startPosition,currencyIcon,targetIcon,i);
         }
     }
 
-    private void SpawnCurrencyIcon(
-        Vector3 startPosition,
-        Sprite currencyIcon,
-        int index)
+    private void SpawnCurrencyIcon(Vector3 startPosition,Sprite currencyIcon,RectTransform targetIcon,int index)
     {
-        GameObject currencyObject = Instantiate(
-            flyingCurrencyIconPrefab,
-            flyingCurrencyPrefabsContainer
-        );
+        GameObject currencyObject = Instantiate(flyingCurrencyIconPrefab,flyingCurrencyPrefabsContainer);
 
-        RectTransform currencyRect =
-            currencyObject.GetComponent<RectTransform>();
+        RectTransform currencyRect = currencyObject.GetComponent<RectTransform>();
 
-        Image currencyImage =
-            currencyObject.GetComponent<Image>();
+        Image currencyImage = currencyObject.GetComponent<Image>();
 
         currencyImage.sprite = currencyIcon;
 
         currencyRect.position = startPosition;
         currencyRect.localScale = Vector3.zero;
 
-        Vector3 scatterPosition =
-            startPosition +
-            new Vector3(
-                Random.Range(-50f, 50f),
-                Random.Range(-30f, 60f),
-                0f
-            );
+        Vector3 scatterPosition = startPosition + new Vector3(UnityEngine.Random.Range(-50f, 50f),UnityEngine.Random.Range(-30f, 60f),0f);
 
         Sequence sequence = DOTween.Sequence();
 
@@ -90,5 +95,59 @@ public class CurrencyVFXSystem : MonoBehaviour
                 .DOMove(scatterPosition, scatterDuration)
                 .SetEase(Ease.OutQuad)
         );
+
+        sequence.Append(
+            currencyRect
+                .DOMove(targetIcon.position, flyDuration)
+                .SetEase(Ease.InQuad)
+        );
+
+        sequence.Join(
+            currencyRect
+                .DOScale(0.6f, flyDuration)
+                .SetEase(Ease.InQuad)
+        );
+
+        sequence.OnComplete(() =>
+        {
+            PulseTarget(targetIcon);
+            Destroy(currencyObject);
+        });
     }
+
+    private RectTransform GetTargetIcon(CurrencyType currencyType)
+    {
+        foreach (CurrencyVFXTarget target in currencyTargets)
+        {
+            if (target.currencyType == currencyType)
+                return target.targetIcon;
+        }
+
+        Debug.LogWarning($"No VFX target found for currency: {currencyType}");
+
+        return null;
+    }
+
+    private void PulseTarget(RectTransform targetIcon)
+    {
+        if (!targetOriginalScales.TryGetValue(targetIcon, out Vector3 originalScale))
+            return;
+
+        targetIcon.DOKill();
+        targetIcon.localScale = originalScale;
+
+        targetIcon
+            .DOPunchScale(originalScale * 0.3f,0.2f,4,0.5f)
+            .OnComplete(() =>
+            {
+                targetIcon.localScale = originalScale;
+            });
+    }
+}
+
+[Serializable]
+public class CurrencyVFXTarget
+{
+    public CurrencyType currencyType;
+    public RectTransform targetIcon;
 }
